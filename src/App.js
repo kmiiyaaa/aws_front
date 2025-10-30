6c20c3733345ada9a91790579e66765a6c0efed0
kmiiyaaa <ckm1533@naver.com>
2025-10-30 17:40:25 +0900

    jwt 사용시 front 수정

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [token, setToken] = useState("" || localStorage.getItem("token")); //토큰값이 있으면 토큰값넣고 없으면 ""
+  // localStorage → 웹브라우저에서 기본적으로 가지고 있는 저장소
 
   //회원 가입
   const signUp = async (e) => {
-    //e.preventDefault();
-
     try {
       await api.post(
         "/api/auth/signup",
-        new URLSearchParams({ username, password })
+        { username, password } // @requestbody로 보냄 -> Json타입으로
+        //new URLSearchParams({ username, password }) //백엔드에서 @requestParam -> 파라미터 넘기기
       );
     } catch (err) {
       console.error();
@@ -37,11 +38,14 @@ function App() {
   //로그인
   const login = async (e) => {
     try {
-      await api.post(
+      const res = await api.post(
         "/api/auth/login",
-        new URLSearchParams({ username, password })
+        { username, password }
+        //new URLSearchParams({ username, password })
       );
-      setMessage(username + "☆ 로그인 성공 :)");
+      setToken(res.data.token); // 로그인 성공 후 받은 토큰값 저장
+      localStorage.setItem("token", res.data.token);
+      setMessage(username + " 로그인 성공 :)");
     } catch (err) {
       console.error(err);
       alert("로그인 실패");
@@ -49,15 +53,34 @@ function App() {
   };
 
   //로그아웃
-  const logout = async (e) => {
-    await api.post("/api/auth/logout");
-    setMessage("★" + username + "★ 로그아웃");
+  const logout = async () => {
+    //await api.post("/api/auth/logout");
+    localStorage.removeItem("token"); // 토큰삭제 → 로그아웃
+    setToken(""); // 토큰값 초기화
+    setMessage("★ " + username + " 로그아웃 ★");
   };
 
   //로그인한 사용자 확인
   const userloginCheck = async () => {
-    const res = await api.get("/api/auth/me");
-    setMessage("현재 로그인한 사용자는 " + res.data.username + "님♡");
+    // const res = await api.get("/api/auth/me");
+    // setMessage("현재 로그인한 사용자는 " + res.data.username + "님♡");
+
+    //JWT 적용시
+    try {
+      if (!token) {
+        // 참이면 로그인 x
+        alert("로그인 후 정보확인 가능합니다.");
+      }
+
+      const res = await api.get("/api/auth/me", {
+        headers: { Authorization: `Bearer {token}` },
+      });
+
+      setMessage("현재 로그인한 사용자는 " + res.data.username + "님♡");
+    } catch (err) {
+      console.error(err);
+      alert("로그인 중인 사용자 정보를 가져올 수 없습니다.");
+    }
   };
 
   return (
--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,7 +4,7 @@ const api = axios.create({
   //baseURL: "http://http://43.200.232.87/", //스프링 부트 백엔드 기본 url
   baseURL: "http://localhost:7777",
   //ec2 아이피:포트
-  withCredentials: true, //세션 쿠키 전달
+  // withCredentials: true, //세션 쿠키 전달  - jwt 할때는 필요없음
 });
 
 export default api;
